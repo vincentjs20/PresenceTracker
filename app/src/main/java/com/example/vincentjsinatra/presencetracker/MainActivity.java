fa640ca9ecb96331d69d534a1a948d5100f561dc
Unknown <vincentyoelsinatra2@gmail.com>
2018-09-12 16:54:58 +0700

    OnActivity

--- app/src/main/java/com/example/vincentjsinatra/presencetracker/MainActivity.java
+++ app/src/main/java/com/example/vincentjsinatra/presencetracker/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.vincentjsinatra.presencetracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    private static final int LOGIN_PERMISSION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnLogin = findViewById(R.id.btnSignIn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAllowNewEmailAccounts(true).build(), LOGIN_PERMISSION
+                );
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==LOGIN_PERMISSION){
+            startNewActivity(resultCode, data);
+        }
+    }
+
+    private void startNewActivity(int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            
+        }
     }
 }
